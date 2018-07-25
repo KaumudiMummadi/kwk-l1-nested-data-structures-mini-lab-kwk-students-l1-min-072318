5b6cecba6aacc954f79f0247427924c0b4415b8a
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-25 21:20:11 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -10,30 +10,30 @@ def sandwich_request(ingredients, order)
   ingredients[0][1]
 end
 
-# example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+# # example city_info hash
+# cities = {
+#   :new_york => { 
+#     :mayor => "Bill DeBlasio",
+#     :population => 8337000,
+#     :website => "http://www.nyc.gov"
+#   },
+#   :los_angeles => { 
+#     :mayor => "Eric Garcetti",
+#     :population => 3884307,
+#     :website => "http://www.lacity.org"
+#   },
+#   :miami => { 
+#     :mayor => "Tomás Regalado",
+#     :population => 419777,
+#     :website => "http://www.miamigov.com"
+#   },
+#   :chicago => { 
+#     :mayor => "Rahm Emanuel",
+#     :population => 2695598,
+#     :website => "http://www.cityofchicago.org/"
+#   }
+# }
 
-def city_populations(cities)
-  # your code here!
-end
+# def city_populations(cities)
+#   # your code here!
+# end
